757cf00b69bf650be2426c4172d53dff3f89ed58
Doug Pilcher <dpilcher.dev@gmail.com>
2023-09-20 16:46:54 -0500

    add favicon and logo

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
+import Image from "next/image";
+
 const Layout = ({children}) => {
   return (
     <html lang="eng">
       <body>
         <header>
-          <h1>Flightless Nerd</h1>
+          <h1 style={{display: "flex", justifyContent: "start", alignItems: "center"}}>
+            <Image src="/Austrich_circle_cropped.png" width="48" height="48" /> Flightless Nerd
+          </h1>
         </header>
       
         {children}
