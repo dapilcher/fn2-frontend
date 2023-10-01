43ce69c4d304b5610679c852c13eeae2ab2e4743
Doug Pilcher <dpilcher.dev@gmail.com>
2023-10-01 00:12:49 -0500

    fixed fonts

--- app/layout.js
+++ app/layout.js
@@ -2,18 +2,22 @@ import './index.css';
 import Header from '../components/Header';
 import { loadDevMessages, loadErrorMessages } from "@apollo/client/dev";
 
-import { Montserrat, Manrope } from '@next/font/google';
+import { Red_Hat_Display, Manrope } from "next/font/google";
 import Footer from '../components/Footer';
 import BodyContainer from '../components/BodyContainer';
 
-export const displayFont = Montserrat({
+export const displayFont = Red_Hat_Display({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-display',
+  weight: ['300', '400', '500', '700', '900'],
+  adjustFontFallback: false
 })
 export const bodyFont = Manrope({
   subsets: ['latin'],
   variable: '--font-body',
+  weight: ['300', '400', '500', '700'],
+  adjustFontFallback: false
 })
 
 
@@ -24,7 +28,7 @@ const Layout = ({children}) => {
   }
 
   return (
-    <html lang="eng">
+    <html lang="eng" className={`${displayFont.variable} ${bodyFont.variable}`}>
       <body className="bg-secondary-50 font-body min-h-screen w-screen flex justify-center">
         <BodyContainer>
           <Header />
--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="py-5 w-screen z-10 flex justify-start">
+    <footer className="py-5 flex justify-start border-t-2 border-t-grey-300">
       <p>© {moment().year()} Flightless Nerd, All Rights Reserved</p>
     </footer>
   );
